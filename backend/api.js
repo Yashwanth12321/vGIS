715f73d2de4e53670b741f5d2b95ff2ef6437997
Yashwanth Napa <yashwanthnapa@gmail.com>
2024-01-27 17:34:42 +0530

    managed some api calls

--- backend/api.js
+++ backend/api.js
@@ -1,30 +1,38 @@
 import axios from 'axios';
+import fs from 'fs';
+
 import { getToken } from './auth.js';
+const REST_KEY=process.env.REST_KEY;
 
 export const makeAPIRequest = async () => {
     const authToken = await getToken();
+
     if (authToken) {
         try {
-            const response = await axios({
-                method: 'post',
-                url: 'https://atlas.mappls.com/api/places/geocode',
+            const response = await axios.request({
+                method: 'get',
+                url: ' https://apis.mapmyindia.com/advancedmaps/v1/'+REST_KEY+'/still_image',
                 headers: {
                     Authorization: `Bearer ${authToken}`,
-                    'Content-Type': 'application/json' // Add 'Content-Type' header
+                    'Content-Type': 'image/png' // Add 'Content-Type' header
                 },
                 params: {
-                    address: 'Delhi'
-                }
+                    center: '28.5959394,77.2255611'
+                },
+                responseType: 'arraybuffer' // Make sure the response type is arraybuffer
+
             });
 
             // Handle the response data
-            console.log(response.data);
+            fs.writeFile('output.png', response.data, (err) => {
+                if (err) throw err;
+                console.log('Image saved!');
+            });
         } catch (error) {
             // Handle the error
             console.error("error");
         }
     } else {
-        // Handle the case when the token is not available or failed to generate
-        // ...
+        console.log("No token found",authToken);
     }
 };
